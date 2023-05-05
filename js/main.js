df50dbf8eec4de1d9578f676109db55fb09b4522
angalland <angalland@laposte.net>
2023-05-05 15:31:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let message = "Class : carre\n- Background color : rgb (22, 228, 166)\n- Color : white\n- Height : 200\n- Width : 200\n- Display : flex\n- Police : Arial\n- Font-size : 2em" 
+
+// function boiteDeDialogue () {
+//     window.alert(message);
+// }
+
+
+
+document.getElementById("carre").addEventListener("click", function () {
+    window.alert(message);
+});
