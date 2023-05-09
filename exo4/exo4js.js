7726145018a96346eff5d5693c74af8adfb66dc0
angalland <angalland@laposte.net>
2023-05-09 17:08:01 +0200

    ajout tableau carre

--- exo4/exo4js.js
+++ exo4/exo4js.js
@@ -21,17 +21,22 @@
     // } else if (event.key === 'ArrowUp')
     //     (".carre").unwrap("article");
 
+let nbCarre = [];
+
 function addCarre () {
     let carre = document.createElement("div");
     carre.classList.add("carre");
     let article = document.querySelector("article")
     article.appendChild(carre);
     carre.style.backgroundColor = couleurAleatoire();
+    nbCarre.push(carre);
 };
 
 function enleverCarre() {
+    if (nbCarre.length > 0) {
+    let carre = nbCarre.pop();
     document.getElementsByName("article").removeChild(carre);
-}
+}}
 
 function couleurAleatoire(){
     let aleatoire = (Math.random() * 0xFFFFFF << 0).toString(16); 
