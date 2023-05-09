633a19947a226dde2e394441daa6bd2c0d7fdb78
angalland <angalland@laposte.net>
2023-05-09 14:30:48 +0200

    ajout fonction partie terminer

--- exo3/exo3js.js
+++ exo3/exo3js.js
@@ -6,12 +6,10 @@ const article = document.querySelector("article");
 for (i = 1; i <= 9; i++) {
     let newcarre = carre.cloneNode();
     article.appendChild(newcarre);
+    
 }
 
 let nb = 0 ;
-
-const cellules = document.querySelectorAll(".carre");
-
 function tour() {
     nb++;
     return nb % 2 === 0 ? "O" : "X";
@@ -21,8 +19,30 @@ function jouer() {
     if(!this.innerHTML) this.innerHTML = tour ();
 }
 
-cellules.forEach(el => el.addEventListener("click", jouer));
+let cellules = document.querySelectorAll(".carre");
 
+
+let joeurTour = document.createElement("h1");
+article.appendChild(joeurTour);
+
+
+function aQuiTour(){
+    if (this.innerHTML == "X") {
+        return joeurTour.innerHTML = "C'est au joeur O de jouer !"
+    } else  {
+        return joeurTour.innerHTML = "C'est au joeur X de jouer !"
+    }
+}
+
+function partieTerminer () {
+    if (cellules[0].innerHTML !== '' && cellules[1].innerHTML !== '' && cellules[2].innerHTML !== '' && cellules[3].innerHTML !== '' && cellules[4].innerHTML !== '' && cellules[5].innerHTML !== '' && cellules[6].innerHTML !== '' && cellules[7].innerHTML !== '' && cellules[8].innerHTML !== '') {
+        return joeurTour.innerHTML = "La partie est terminé !"
+    }
+}
+
+cellules.forEach(el => el.addEventListener("click", jouer));
+cellules.forEach(el => el.addEventListener("click", aQuiTour));
+cellules.forEach(el => el.addEventListener("click", partieTerminer));
 
 
 
