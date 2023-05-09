5e0fa0105e4ed09db21ad27c4f2da77a2374ce0d
angalland <angalland@laposte.net>
2023-05-09 16:51:53 +0200

    ajout de la fonction addCarre et couleurAleatoire

--- exo4/exo4js.js
+++ exo4/exo4js.js
@@ -1,22 +1,41 @@
-const carre = document.createElement("div");
-carre.classList.add("carre");
-const article = document.querySelector("article");
+// const carre = document.createElement("div");
+// carre.classList.add("carre");
+// const article = document.querySelector("article");
 
 
-window.addEventListener("keydown", function (event) {
-    if (event.key === 'ArrowDown'){
+// window.addEventListener("keydown", function (event) {
+//     if (event.key === 'ArrowDown'){
         
-        let newcarre = carre.cloneNode();
-        article.appendChild(newcarre);
-        
-        
-        let aleatoire = (Math.random() * 0xFFFFFF << 0).toString(16);
-        let couleurAleatoire = document.querySelectorAll(".carre");
-        
-        couleurAleatoire.style.backgroundColor = "#" + aleatoire;
+//         let newcarre = carre.cloneNode();
+//         article.appendChild(newcarre);
         
         
-    }});
+//         let aleatoire = (Math.random() * 0xFFFFFF << 0).toString(16);
+//         let couleurAleatoire = document.querySelectorAll(".carre");
+        
+//         couleurAleatoire.style.backgroundColor = "#" + aleatoire;
+        
+        
+//     }});
     
     // } else if (event.key === 'ArrowUp')
     //     (".carre").unwrap("article");
+
+function addCarre () {
+    let carre = document.createElement("div");
+    carre.classList.add("carre");
+    let article = document.querySelector("article")
+    article.appendChild(carre);
+    carre.style.backgroundColor = couleurAleatoire();
+};
+
+function couleurAleatoire(){
+    let aleatoire = (Math.random() * 0xFFFFFF << 0).toString(16); 
+    return "#" + aleatoire;
+}
+
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowDown") {
+        addCarre();
+    }
+})
