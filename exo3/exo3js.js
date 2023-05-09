3015c71587b8b3df29def517ba8b1535b7f753c3
angalland <angalland@laposte.net>
2023-05-09 13:44:37 +0200

    ajout fonction tour et jouer

--- exo3/exo3js.js
+++ exo3/exo3js.js
@@ -6,22 +6,23 @@ const article = document.querySelector("article");
 for (i = 1; i <= 9; i++) {
     let newcarre = carre.cloneNode();
     article.appendChild(newcarre);
-
-    let joeur1 = "X";
-    let joeur2 = "O";
-    let tourJoeur = joeur1;
-
-    newcarre.addEventListener("click", function(event) {
-
-            event.target.innerText = tourJoeur;
-            if (tourJoeur == joeur1) {
-                tourJoeur = joeur2;
-            } else {
-                tourJoeur = joeur1;
-            }
-    })
-
-   
 }
+
+let nb = 0 ;
+
+const cellules = document.querySelectorAll(".carre");
+
+function tour() {
+    nb++;
+    return nb % 2 === 0 ? "O" : "X";
+}
+
+function jouer() {
+    if(!this.innerHTML) this.innerHTML = tour ();
+}
+
+cellules.forEach(el => el.addEventListener("click", jouer));
+
+
 
 
